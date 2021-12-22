4f8eb7ee3bda64384043a18787d6039ab29e2a6b
theone <tanwei.rush@gmail.com>
2021-12-22 11:14:03 +0800

    1. 指定打开的文件 2. 3. 4.

--- example/main.go
+++ example/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
 	"io/ioutil"
+	"os"
 
 	"github.com/sunreaver/sqlsplit"
 )
 
 func main() {
-	data, _ := ioutil.ReadFile("example.sql")
+	f := flag.String("f", "example.sql", "解析的文件")
+	if len(*f) == 0 {
+		os.Exit(1)
+	}
+	data, e := ioutil.ReadFile(*f)
+	if e != nil {
+		fmt.Printf("打开文件 %v 失败: %v", *f, e.Error())
+		os.Exit(1)
+	}
 	outs := sqlsplit.Split(string(data))
 	fmt.Printf("共解析出%v条sql\n", len(outs))
 	for idx, v := range outs {
