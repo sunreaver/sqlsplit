eafb8006c1e372d041fb8de636e95fa5ced689cd
theone <tanwei.rush@gmail.com>
2022-07-25 09:34:26 +0800

    1. 添加单元测试 2. 添加IF()用例 3. 4.

---
+++ parse_test.go
@@ -0,0 +1,24 @@
+package sqlsplit
+
+import (
+	"io/ioutil"
+	"testing"
+)
+
+var insqls = []byte{}
+
+func TestMain(m *testing.M) {
+	var err error
+	insqls, err = ioutil.ReadFile("example/example.sql")
+	if err != nil {
+		panic(err)
+	}
+	m.Run()
+}
+
+func TestSplit(t *testing.T) {
+	ps := Split(string(insqls)) // 测试分词
+	if len(ps) != 49 {
+		t.Errorf("split error, expect 49, got %d", len(ps))
+	}
+}
